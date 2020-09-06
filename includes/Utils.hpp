f3199f42b7b6ed81d58ece251e080303835ef7eb
ereynier <ereynier@student.le-101.fr>
2020-09-06 18:44:54 +0200

    base Parser + parser Date

--- includes/Utils.hpp
+++ includes/Utils.hpp
@@ -12,7 +12,6 @@ std::string					make_html_error_page(int error_code, const std::string& error_st
 std::string					get_gmt_time(time_t date);
 void						http_print(const std::string& s);
 void						dec_print(const char *s);
-
 std::vector<std::string>    header_finder(const std::vector<std::string> lines, const std::string to_find);
 std::vector<std::string>    get_header_name(std::string str, char c);
 bool                        is_number(const std::string s);
