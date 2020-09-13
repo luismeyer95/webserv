05eba8899b8ebe571f5e754bb0f9ced8be4a6837
ereynier <ereynier@student.le-101.fr>
2020-09-13 21:54:44 +0200

    ResponseConstructor lil modif

--- srcs/ResponseConstructor.cpp
+++ srcs/ResponseConstructor.cpp
@@ -16,5 +16,36 @@ ResponseConstructor::~ResponseConstructor()
 
 void ResponseConstructor::constructor(RequestParser req)
 {
+    if (req.getMethod() == "GET")
+    {
 
+    }
+    else if (req.getMethod() == "POST")
+    {
+
+    }
+    else if (req.getMethod() == "HEAD")
+    {
+
+    }
+    else if (req.getMethod() == "PUT")
+    {
+        
+    }
+    else if (req.getMethod() == "DELETE")
+    {
+        
+    }
+    else if (req.getMethod() == "CONNECT")
+    {
+        
+    }
+    else if (req.getMethod() == "OPTIONS")
+    {
+        
+    }
+    else if (req.getMethod() == "TRACE")
+    {
+        
+    }
 }
